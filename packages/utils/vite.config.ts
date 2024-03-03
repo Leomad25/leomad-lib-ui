2033c02535487868170f119985819bddee84feaf
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-03-03 06:16:53 -0500

    main monorepo

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'LeomadLibUI',
+      fileName: 'leomad-lib-ui',
+      formats: ['es', 'cjs', 'umd', 'iife']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
